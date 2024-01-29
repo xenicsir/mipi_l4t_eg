e935af580eff4075f2cc32a61ce4a3e5ba23d1d0
Cyril GERMAINE <cyril.germaine@device-alab.com>
2024-01-29 18:25:47 +0100

    Fixed Auvidea patch

--- l4t_apply_patches.sh
+++ l4t_apply_patches.sh
@@ -6,11 +6,11 @@ patch -N -d $L4T_VERSION/Linux_for_Tegra/source/public/hardware/ -p1 < patches/$
 
 if [[ x$2 == xauvidea ]]
 then
-   if [[ -f patches/$L4T_VERSION/auvidea_*.patch ]]
-   then
+#   if [[ -f patches/$L4T_VERSION/auvidea_*.patch ]]
+#   then
       patch -N -d $L4T_VERSION/Linux_for_Tegra/ -p1 < patches/$L4T_VERSION/auvidea_l4t.patch
       patch -N -d $L4T_VERSION/Linux_for_Tegra/source/public/hardware/ -p1 < patches/$L4T_VERSION/auvidea_l4t_source_public_hardware.patch
-   fi
+#   fi
 fi
 
 
